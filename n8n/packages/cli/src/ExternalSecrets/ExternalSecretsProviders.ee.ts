257d133e2aaafe9aeafb6d30ad08c0f2d6896d92
Dharshan senthil <131781900+Dharshan209@users.noreply.github.com>
2025-10-03 05:23:17 +0530

    Remove vault provider reference

--- n8n/packages/cli/src/ExternalSecrets/ExternalSecretsProviders.ee.ts
+++ n8n/packages/cli/src/ExternalSecrets/ExternalSecretsProviders.ee.ts
@@ -1,13 +1,11 @@
 import type { SecretsProvider } from '@/Interfaces';
 import { Service } from 'typedi';
 import { InfisicalProvider } from './providers/infisical';
-import { VaultProvider } from './providers/vault';
 
 @Service()
 export class ExternalSecretsProviders {
 	providers: Record<string, { new (): SecretsProvider }> = {
 		infisical: InfisicalProvider,
-		vault: VaultProvider,
 	};
 
 	getProvider(name: string): { new (): SecretsProvider } | null {
